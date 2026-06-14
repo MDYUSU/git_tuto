1f88899d3f912f068f085a9c00f6897e914599b5
Mohammed Yusuf <mdyusuf0210@gmail.com>
2026-06-14 23:49:47 +0530

    feat: update role to full stack developer

--- about.js
+++ about.js
@@ -1,7 +1,7 @@
 const about = {
   name: "Mohammed Yusuf",
-  role: "MERN Stack Developer",
-  location: "Udupi, Karnataka",
-};
+  role: "Full Stack Developer",
+  location: "Udupi, Karnataka"
+}
 
-module.exports = about;
+module.exports = about
